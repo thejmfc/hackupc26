85ec518201014aa25250136dd84a97a1f442bbbf
Ben Lamb <benandrewlamb@gmail.com>
2026-04-25 12:11:50 +0200

    Added toggleable styles for highlighting the map

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { useRef, useEffect } from 'react';
+import { useRef, useEffect, useState } from 'react';
 import mapboxgl from 'mapbox-gl';
 
 import 'mapbox-gl/dist/mapbox-gl.css';
@@ -6,20 +6,29 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 function App() {
     const mapRef = useRef();
     const mapContainerRef = useRef();
+    const [isHighlighted, setIsHighlighted] = useState(false);
+
+    const toggleHighlight = () => {
+        setIsHighlighted(!isHighlighted);
+    };
 
     useEffect(() => {
         mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_API_TOKEN;
         mapRef.current = new mapboxgl.Map({
             container: mapContainerRef.current,
         });
-
+        mapRef.current.setStyle(isHighlighted 
+            ? import.meta.env.VITE_HIGHLIGHT_URL
+            : import.meta.env.VITE_BLANK_URL
+        );
         return () => {
             mapRef.current.remove()
         }
-    }, []);
+    }, [isHighlighted]);
 
     return (
         <>
+          <button onClick={toggleHighlight}>Toggle Highlight</button>
           <div id="map-container" ref={mapContainerRef} />
         </>
     );

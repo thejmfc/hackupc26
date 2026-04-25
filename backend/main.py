2f93578c00ee8dc4d35be00a22bdc7e34155d5e8
Harry Clayton <hharry.jack.clayton@gmail.com>
2026-04-25 15:44:21 +0200

    Yes
    yes

--- backend/main.py
+++ backend/main.py
@@ -23,30 +23,6 @@ async def generate(prompt: str):
     print(f"Received prompt: {prompt}")
     return generate_with_prompt(prompt)
 
-# @app.get("/flights")
-# async def flights(origin: str, destination: str, year: int, month: int, day: int):
-#     payload = {
-#         "query": {
-#             "market": "UK",
-#             "locale": "en-GB",
-#             "currency": "GBP",
-#             "queryLegs": [
-#                 {
-#                     "originPlaceId": {"iata": origin},
-#                     "destinationPlaceId": {"iata": destination},
-#                     "date": {"year": year, "month": month, "day": day},
-#                 }
-#             ],
-#             "adults": 1,
-#             "cabinClass": "CABIN_CLASS_ECONOMY",
-#         }
-#     }
 
-#     async with httpx.AsyncClient(timeout=30) as client:
-#         response = await client.post(
-#             f"{BASE_URL}/flights/live/search/create", headers=HEADERS, json=payload
-#         )
-#         response.raise_for_status()
-#         return response.json()
 
 
